5c324932dfc515ede2d11fed991739b6072c1d7a
Aleks <generalovshura9@gmail.com>
2023-11-25 12:23:27 +0200

    i have gotten actuallity information from my array

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,77 +1,41 @@
 import "./card.sass";
+import "../../sass/variables.sass";
 import img from "../../resources/img/m55-right.jpg";
+import data from "../../tiresArray";
+import { useEffect, useState } from "react";
 
 const Card = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(data);
+  }, []);
+
   return (
     <>
-      <main>
-        <div className="container">
-          <div className="text-center">
-            <div className="row row-cols-4">
+      {cards.map((card, i) => (
+        <main key={i}>
+          <div className="container">
+            <div className="row">
               <div className="col">
                 <div className="card">
                   <img src={img} className="card-img-top" alt="tires" />
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">title</li>
-                    <li className="list-group-item">price</li>
-                    <li className="list-group-item">seezon</li>
+                  <ul className="list-group">
+                    <li className="list-group-item">{card.title}</li>
+                    <li className="list-group-item">{card.price}</li>
+                    <li className="list-group-item">{card.description}</li>
                   </ul>
                   <div className="card-body">
                     <a href="#" className="card-link">
-                      Card link
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col">
-                <div className="card">
-                  <img src={img} className="card-img-top" alt="tires" />
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">title</li>
-                    <li className="list-group-item">price</li>
-                    <li className="list-group-item">seezon</li>
-                  </ul>
-                  <div className="card-body">
-                    <a href="#" className="card-link">
-                      Card link
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col">
-                <div className="card">
-                  <img src={img} className="card-img-top" alt="tires" />
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">title</li>
-                    <li className="list-group-item">price</li>
-                    <li className="list-group-item">seezon</li>
-                  </ul>
-                  <div className="card-body">
-                    <a href="#" className="card-link">
-                      Card link
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card">
-                  <img src={img} class="card-img-top" alt="tires" />
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">title</li>
-                    <li class="list-group-item">price</li>
-                    <li class="list-group-item">seezon</li>
-                  </ul>
-                  <div class="card-body">
-                    <a href="#" className="card-link">
-                      Card link
+                      {card.title}
                     </a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      ))}
     </>
   );
 };
--- src/tiresArray.js
+++ src/tiresArray.js
@@ -1,124 +1,127 @@
-import Header from "./components/Header/Header";
-import Card from "./components/Card/Card";
-import Pagination from "./components/Pagination/Pagination";
-import CardItem from "./components/CardItem/CardItem";
-
 const tiresArray = [
   {
     title: "RoadMaster Plus",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/a71/2cf/thumb_198153_190_200_0_0_crop.jpg",
     price: 120,
     description: "Шины для повседневного использования на городских дорогах.",
   },
   {
-    title: "All-Terrain Beast",
+    title: "All-Terrain Beag",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/db2/478/thumb_198191_190_200_0_0_crop.jpg",
     price: 180,
     description: "Универсальные шины для бездорожья и пересеченной местности.",
   },
   {
     title: "EcoDrive Saver",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/40b/c05/thumb_198206_190_200_0_0_crop.jpg",
     price: 100,
     description:
       "Экономичные шины с улучшенным сцеплением для экологичного вождения.",
   },
   {
     title: "WinterFrost Grip",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/be2/880/thumb_198231_190_200_0_0_crop.jpg",
     price: 150,
     description: "Зимние шины с отличной устойчивостью на скользких дорогах.",
   },
   {
     title: "MaxMileage Runner",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/85f/390/thumb_198238_190_200_0_0_crop.jpg",
     price: 140,
     description: "Шины с максимальным пробегом для длительного использования.",
   },
   {
     title: "CityCruiser Touring",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/d97/a04/thumb_198622_190_200_0_0_crop.jpg",
     price: 130,
     description: "Шины для комфортной езды по городским улицам.",
   },
   {
     title: "Off-Road Dominator",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/192/0ef/thumb_198834_190_200_0_0_crop.jpg",
     price: 190,
     description: "Прочные шины для бездорожья с повышенной проходимостью.",
   },
   {
     title: "Performance Plus",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/021/a8a/thumb_198880_190_200_0_0_crop.jpg",
     price: 200,
     description:
       "Шины для высокой производительности и улучшенного сцепления с дорогой.",
   },
   {
     title: "RainMaster",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/a3a/1e2/thumb_198946_190_200_0_0_crop.jpg",
     price: 160,
     description: "Шины с улучшенной устойчивостью на дождливых дорогах.",
   },
   {
     title: "FuelSaver Elite",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/5e8/91a/thumb_198999_190_200_0_0_crop.jpg",
     price: 170,
     description:
       "Эффективные шины для экономии топлива и минимального сопротивления качению.",
   },
   {
     title: "UltraGrip Snow",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/1a3/133/thumb_198992_190_200_0_0_crop.jpg",
     price: 210,
     description:
       "Шины с высоким уровнем сцепления для безопасной езды на снегу и льду.",
   },
   {
     title: "ComfortCruise",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/533/5d0/thumb_199078_190_200_0_0_crop.jpg",
     price: 150,
     description:
       "Комфортные шины с улучшенной амортизацией для плавной поездки.",
   },
   {
     title: "RuggedTrail Blazer",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/a40/5cf/thumb_199109_190_200_0_0_crop.png",
     price: 190,
     description: "Прочные шины для внедорожников с повышенной устойчивостью.",
   },
   {
     title: "StreetSport Pro",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/ddd/480/thumb_199357_190_200_0_0_crop.jpg",
     price: 220,
     description:
       "Шины для спортивных автомобилей с высокими показателями сцепления.",
   },
   {
     title: "CityStar Deluxe",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/2f1/551/thumb_204797_190_200_0_0_crop.jpg",
     price: 130,
     description:
       "Долговечные шины для городского транспорта с повышенным комфортом.",
   },
   {
     title: "TerrainMaster XT",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/477/b72/thumb_199775_190_200_0_0_crop.jpg",
     price: 200,
     description: "Универсальные шины для экстремальных условий на дороге.",
   },
   {
     title: "EcoMax Fuel Saver",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/225/e80/thumb_204923_190_200_0_0_crop.jpg",
     price: 170,
     description:
       "Экономичные шины для снижения расхода топлива и улучшенной устойчивости.",
   },
   {
     title: "WinterPro Grip",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/4e4/d0e/thumb_206458_190_200_0_0_crop.jpg",
     price: 160,
     description: "Зимние шины с превосходным сцеплением на льду и снегу.",
   },
   {
     title: "HighwayHero",
+    img: "https://kolesa-66.ru/storage/app/uploads/public/RTI/107/c50/thumb_206658_190_200_0_0_crop.jpg",
     price: 180,
     description:
       "Надежные шины для путешествий по шоссе с улучшенной долговечностью.",
   },
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Card />
-      <Pagination />
-      {/* <CardItem /> */}
-    </div>
-  );
-}
-
-export default App;
+export default tiresArray;
